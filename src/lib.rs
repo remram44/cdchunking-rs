5476db6f3bbb23fcafed2175bcbf8a0cd2d8ab22
Remi Rampin <remirampin@gmail.com>
2017-08-03 10:38:35 -0400

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,71 @@
-trait ChunkerImpl {
+/// This class is the internal method of finding chunk boundaries.
+///
+/// It can look at the actual bytes or not, for example:
+/// * Use a rolling algorithm such as ZPAQ or Adler32
+/// * Find some predefined boundary in the data
+/// * Make blocks of a fixed size (then it's NOT content-defined!)
+///
+/// This is where the internal state of the algorithm should be kept (counter,
+/// hash, etc).
+pub trait ChunkerImpl {
+    /// Look at the new bytes to maybe find a boundary.
+    fn find_boundary(&mut self, data: &[u8]) -> Some(usize);
 }
 
-struct Chunker<I: ChunkerImpl> {
+/// Chunker object, wraps the rolling hash into a stream-splitting object.
+pub struct Chunker<I: ChunkerImpl> {
     inner: I,
 }
 
 impl<I: ChunkerImpl> Chunker<I> {
+    /// Create a Chunker from a specific way of finding chunk boundaries.
+    pub fn new(inner: I) -> Chunker<I> {
+        Chunker { inner: inner }
+    }
+
+    pub fn whole_chunks<R: Read>(&mut self, reader: R) -> WholeChunks {
+        unimplemented!()
+    }
+
+    pub fn all_chunks<R: Read>(&mut self, reader: R)
+        -> io::Result<Vec<Vec<u8>>>
+    {
+        let mut chunks = Vec::new();
+        for chunk in self.whole_chunks(reader) {
+            match chunk {
+                Ok(chunk) => chunks.push(chunk),
+                Err(e) => return Err(e)
+            }
+        }
+        Ok(chunks)
+    }
+
+    pub fn stream<R: Read>(&mut self, reader: R) -> ChunkStream {
+        unimplemented!()
+    }
+
+    pub fn chunks<R: IntoRead>(&mut self, reader: R) -> ChunkInfoStream {
+        let mut reader = reader.into_reader();
+
+        let mut pos = 0;
+        let mut last_chunk = 0;
+        let mut chunk_iter = self.stream(reader);
+        while let Some(chunk) = chunk_iter.read() {
+            let chunk = chunk.unwrap();
+            match chunk {
+                ChunkInput::Data(d) => pos += d.len(),
+                ChunkInput::End => {
+                    yield ChunkInfo { start: last_chunk,
+                                      length: pos - last_chunk }
+                    last_chunk = pos;
+                }
+            }
+        }
+    }
+
+    pub fn slices(&mut self, buffer: &[u8]) -> Slices {
+        unimplemented!()
+    }
 }
 
 #[test]
